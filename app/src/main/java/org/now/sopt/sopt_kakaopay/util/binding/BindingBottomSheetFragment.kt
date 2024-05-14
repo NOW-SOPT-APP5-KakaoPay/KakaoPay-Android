84aa4f92b2260ad5297a9a4235ecc8388147f00f
Moonsu Kang <rkdms4971@naver.com>
2024-05-14 15:52:40 +0900

    [FIX/#1] 기초세팅 추가 및 수정

--- app/src/main/java/org/now/sopt/sopt_kakaopay/util/binding/BindingBottomSheetFragment.kt
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/util/binding/BindingBottomSheetFragment.kt
@@ -7,16 +7,15 @@ import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-abstract class BindingFragment<T : ViewDataBinding>(
+abstract class BindingBottomSheetFragment<T : ViewDataBinding>(
     @LayoutRes private val layoutResId: Int // Layout Resource ID를 인자로 받음
-) : Fragment() {
+) : BottomSheetDialogFragment() {
     private var _binding: T? = null // ViewDataBinding 객체를 null로 초기화
 
     protected val binding: T // binding 프로퍼티를 통해 _binding에 접근, null일 경우 예외 발생
         get() = requireNotNull(_binding) { "바인딩 객체 좀 생성해주세요 제발!!" }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,16 +25,15 @@ abstract class BindingFragment<T : ViewDataBinding>(
         binding.lifecycleOwner = viewLifecycleOwner // lifecycleOwner를 현재 Fragment의 viewLifecycleOwner로 설정
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
     }
+
     protected abstract fun initView()
 
-    // 프래그먼트의 뷰가 파괴될 때 호출되는 메서드
     override fun onDestroyView() {
-        _binding = null // _binding = null로 설정해서 메모리 누수 방지
+        _binding = null
         super.onDestroyView()
     }
 }

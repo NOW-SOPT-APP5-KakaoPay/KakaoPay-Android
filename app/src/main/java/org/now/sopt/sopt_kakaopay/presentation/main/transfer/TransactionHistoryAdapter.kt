725b80914552c5f63901c6a4cdaf364fea3083bc
Moonsu Kang <rkdms4971@naver.com>
2024-05-22 00:08:39 +0900

    [FEAT/#20] 최근 송금계좌 어뎁터 및 뷰홀더 추가

---
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/transfer/TransactionHistoryAdapter.kt
@@ -0,0 +1,26 @@
+package org.now.sopt.sopt_kakaopay.presentation.main.transfer
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import org.now.sopt.sopt_kakaopay.databinding.ItemRecentAccountBinding
+import org.now.sopt.sopt_kakaopay.model.TransactionHistoryDto
+import org.now.sopt.sopt_kakaopay.util.view.ItemDiffCallback
+
+class TransactionHistoryAdapter : ListAdapter<TransactionHistoryDto, TransactionHistoryViewHolder>(
+    ItemDiffCallback(
+        onItemsTheSame = { oldItem, newItem -> oldItem.bankAccount == newItem.bankAccount },
+        onContentsTheSame = { oldItem, newItem -> oldItem == newItem }
+    )
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionHistoryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemRecentAccountBinding.inflate(inflater, parent, false)
+        return TransactionHistoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TransactionHistoryViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+}

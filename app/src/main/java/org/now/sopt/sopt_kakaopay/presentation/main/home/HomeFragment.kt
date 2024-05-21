fa775ceedfd642b9a75484ca8ab5b401f63255fc
yeonjeen <jeendev2019@gmail.com>
2024-05-22 02:19:18 +0900

    fix: 충돌 수정

--- app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/home/HomeFragment.kt
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/home/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import org.now.sopt.sopt_kakaopay.R
 import org.now.sopt.sopt_kakaopay.databinding.FragmentHomeBinding
-import org.now.sopt.sopt_kakaopay.util.binding.BindingFragment
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null

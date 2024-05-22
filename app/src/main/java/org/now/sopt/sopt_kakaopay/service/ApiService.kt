1643b73253f750fe3d2ead38d48b07cddb5caecc
yeonjeen <jeendev2019@gmail.com>
2024-05-22 23:29:52 +0900

    feat: 북마크 추가 api service 추가

--- app/src/main/java/org/now/sopt/sopt_kakaopay/service/ApiService.kt
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/service/ApiService.kt
@@ -1,10 +1,19 @@
 package org.now.sopt.sopt_kakaopay.service
 
+import org.now.sopt.sopt_kakaopay.model.BookMarkRequestDto
+import org.now.sopt.sopt_kakaopay.model.BookMarkResponseDto
 import org.now.sopt.sopt_kakaopay.model.TransactionHistoryResponseDto
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
     @GET("api/v1/history")
     suspend fun fetchTransactionHistory(): Response<TransactionHistoryResponseDto>
+
+    @POST("api/v1/bookmark")
+    suspend fun addBookmark(
+        @Body bookmarkRequest: BookMarkRequestDto
+    ): BookMarkResponseDto
 }

52d595cb8e4914c6622561a000a594f3204c5336
Moonsu Kang <rkdms4971@naver.com>
2024-05-23 11:18:43 +0900

    [FEAT/#21] 요청 추가

--- app/src/main/java/org/now/sopt/sopt_kakaopay/service/ApiService.kt
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/service/ApiService.kt
@@ -1,5 +1,6 @@
 package org.now.sopt.sopt_kakaopay.service
 
+import org.now.sopt.sopt_kakaopay.model.BalanceResponseDto
 import org.now.sopt.sopt_kakaopay.model.TransactionHistoryResponseDto
 import retrofit2.Response
 import retrofit2.http.GET
@@ -7,4 +8,7 @@ import retrofit2.http.GET
 interface ApiService {
     @GET("api/v1/history")
     suspend fun fetchTransactionHistory(): Response<TransactionHistoryResponseDto>
+
+    @GET("api/v1/member/balance")
+    suspend fun fetchBalance(): Response<BalanceResponseDto>
 }

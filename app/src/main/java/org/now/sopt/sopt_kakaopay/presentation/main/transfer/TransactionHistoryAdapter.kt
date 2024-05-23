2880f0cfc31438a4978fc31781b53e5d21ad870c
yeonjeen <jeendev2019@gmail.com>
2024-05-24 00:59:51 +0900

    refactor: 클릭 리스너를 수락하도록 어댑터 수정

--- app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/transfer/TransactionHistoryAdapter.kt
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/transfer/TransactionHistoryAdapter.kt
@@ -7,21 +7,18 @@ import org.now.sopt.sopt_kakaopay.databinding.ItemRecentAccountBinding
 import org.now.sopt.sopt_kakaopay.model.TransactionHistoryDto
 import org.now.sopt.sopt_kakaopay.util.view.ItemDiffCallback
 
-class TransactionHistoryAdapter(private val viewModel: TransferViewModel) :
+class TransactionHistoryAdapter(private val bookmarkClickListener: (TransactionHistoryDto) -> Unit) :
     ListAdapter<TransactionHistoryDto, TransactionHistoryViewHolder>(
-        ItemDiffCallback(
-            onItemsTheSame = { oldItem, newItem -> oldItem.bankAccount == newItem.bankAccount },
-            onContentsTheSame = { oldItem, newItem -> oldItem == newItem }
-        )
+        ItemDiffCallback(onItemsTheSame = { oldItem, newItem -> oldItem.bankAccount == newItem.bankAccount },
+            onContentsTheSame = { oldItem, newItem -> oldItem == newItem })
     ) {
 
     override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
+        parent: ViewGroup, viewType: Int
     ): TransactionHistoryViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemRecentAccountBinding.inflate(inflater, parent, false)
-        return TransactionHistoryViewHolder(binding, viewModel)
+        return TransactionHistoryViewHolder(binding, bookmarkClickListener)
     }
 
     override fun onBindViewHolder(holder: TransactionHistoryViewHolder, position: Int) {

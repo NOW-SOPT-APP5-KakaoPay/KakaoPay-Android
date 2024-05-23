5dc480a5be5e1effc4812fe464b1b96616d6127a
Moonsu Kang <rkdms4971@naver.com>
2024-05-23 11:19:06 +0900

    [FEAT/#21] 내 자산 요청 구현

---
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/assets/AssetsViewModel.kt
@@ -0,0 +1,39 @@
+package org.now.sopt.sopt_kakaopay.presentation.main.assets
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import org.now.sopt.sopt_kakaopay.model.BalanceDto
+import org.now.sopt.sopt_kakaopay.service.ApiService
+import org.now.sopt.sopt_kakaopay.util.view.UiState
+
+class AssetsViewModel(private val apiService: ApiService) : ViewModel() {
+
+    private val _balanceUiState = MutableStateFlow<UiState<BalanceDto>>(UiState.Loading)
+    val balanceUiState: StateFlow<UiState<BalanceDto>> get() = _balanceUiState
+
+    fun fetchBalance() {
+        viewModelScope.launch {
+            _balanceUiState.value = UiState.Loading
+            runCatching {
+                val response = apiService.fetchBalance()
+                if (response.isSuccessful) {
+                    val serverResponse = response.body()
+                    if (serverResponse != null && serverResponse.data.payMoney.isNotEmpty()) {
+                        UiState.Success(serverResponse.data)
+                    } else {
+                        UiState.Empty
+                    }
+                } else {
+                    UiState.Error("Error: ${response.code()}")
+                }
+            }.onSuccess { state ->
+                _balanceUiState.value = state
+            }.onFailure { exception ->
+                _balanceUiState.value = UiState.Error(exception.message)
+            }
+        }
+    }
+}

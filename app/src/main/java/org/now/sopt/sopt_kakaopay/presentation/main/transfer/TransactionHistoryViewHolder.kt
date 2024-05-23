b011b2a1417863a685132839789e6262aa8d6677
yeonjeen <jeendev2019@gmail.com>
2024-05-24 01:16:52 +0900

    refactor: 공백삭제

--- app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/transfer/TransactionHistoryViewHolder.kt
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/transfer/TransactionHistoryViewHolder.kt
@@ -25,8 +25,6 @@ class TransactionHistoryViewHolder(
             ivRecentUserFavorite.setOnClickListener {
                 bookmarkClickListener(transactionHistory)
             }
-
-
         }
     }
 }

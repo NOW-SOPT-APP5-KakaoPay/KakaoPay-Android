3b902256d4e1daba6fcd928ef517389d9b94be88
yeonjeen <jeendev2019@gmail.com>
2024-05-23 11:19:34 +0900

    feat: 어댑터에 뷰모델 선언

--- app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/transfer/TransactionHistoryAdapter.kt
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/transfer/TransactionHistoryAdapter.kt
@@ -7,20 +7,26 @@ import org.now.sopt.sopt_kakaopay.databinding.ItemRecentAccountBinding
 import org.now.sopt.sopt_kakaopay.model.TransactionHistoryDto
 import org.now.sopt.sopt_kakaopay.util.view.ItemDiffCallback
 
-class TransactionHistoryAdapter : ListAdapter<TransactionHistoryDto, TransactionHistoryViewHolder>(
-    ItemDiffCallback(
-        onItemsTheSame = { oldItem, newItem -> oldItem.bankAccount == newItem.bankAccount },
-        onContentsTheSame = { oldItem, newItem -> oldItem == newItem }
-    )
-) {
+class TransactionHistoryAdapter(private val viewModel: TransferViewModel) :
+    ListAdapter<TransactionHistoryDto, TransactionHistoryViewHolder>(
+        ItemDiffCallback(
+            onItemsTheSame = { oldItem, newItem -> oldItem.bankAccount == newItem.bankAccount },
+            onContentsTheSame = { oldItem, newItem -> oldItem == newItem }
+        )
+    ) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionHistoryViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TransactionHistoryViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemRecentAccountBinding.inflate(inflater, parent, false)
-        return TransactionHistoryViewHolder(binding)
+        return TransactionHistoryViewHolder(binding, viewModel)
     }
 
     override fun onBindViewHolder(holder: TransactionHistoryViewHolder, position: Int) {
         holder.onBind(getItem(position))
     }
 }
+
+

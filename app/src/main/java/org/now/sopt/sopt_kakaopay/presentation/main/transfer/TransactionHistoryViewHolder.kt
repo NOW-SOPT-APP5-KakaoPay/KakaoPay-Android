89fc300c9ec566d56a6862cf46688371262de39c
yeonjeen <jeendev2019@gmail.com>
2024-05-24 00:59:16 +0900

    refactor: ViewHolder에서 콜백을 통해 북마크 클릭 이벤트 처리

--- app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/transfer/TransactionHistoryViewHolder.kt
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/transfer/TransactionHistoryViewHolder.kt
@@ -3,12 +3,11 @@ package org.now.sopt.sopt_kakaopay.presentation.main.transfer
 import androidx.recyclerview.widget.RecyclerView
 import org.now.sopt.sopt_kakaopay.R
 import org.now.sopt.sopt_kakaopay.databinding.ItemRecentAccountBinding
-import org.now.sopt.sopt_kakaopay.model.BookMarkRequestDto
 import org.now.sopt.sopt_kakaopay.model.TransactionHistoryDto
 
 class TransactionHistoryViewHolder(
     private val binding: ItemRecentAccountBinding,
-    private val viewModel: TransferViewModel
+    private val bookmarkClickListener: (TransactionHistoryDto) -> Unit
 ) :
     RecyclerView.ViewHolder(binding.root) {
     fun onBind(transactionHistory: TransactionHistoryDto) {
@@ -18,24 +17,17 @@ class TransactionHistoryViewHolder(
             tvRecentAccountUserBank.text = transactionHistory.bank
             ImageUtil.setBankImage(itemView.context, ivRecentUserBankLogo, transactionHistory.bank)
 
-            if (transactionHistory.bookmark) {
-                ivRecentUserFavorite.setImageResource(R.drawable.ic_star_full)
-            } else {
-                ivRecentUserFavorite.setImageResource(R.drawable.ic_star_empty)
-            }
+            ivRecentUserFavorite.setImageResource(
+                if (transactionHistory.bookmark) R.drawable.ic_star_full
+                else R.drawable.ic_star_empty
+            )
 
             ivRecentUserFavorite.setOnClickListener {
-                val bookmarkRequest =
-                    BookMarkRequestDto(transactionHistory.bank, transactionHistory.bankAccount)
-                if (transactionHistory.bookmark) {
-                    viewModel.deleteBookmark(bookmarkRequest)
-                } else {
-                    viewModel.addBookmark(bookmarkRequest)
-                }
+                bookmarkClickListener(transactionHistory)
             }
+
 
         }
     }
-
 }
 

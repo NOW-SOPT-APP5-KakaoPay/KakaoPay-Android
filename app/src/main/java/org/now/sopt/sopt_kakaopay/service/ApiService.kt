0a6a2b90a7f788c7d8108e416dac4ae7a87f536a
yeonjeen <jeendev2019@gmail.com>
2024-05-23 11:17:27 +0900

    feat: 북마크 삭제 api service 추가

--- app/src/main/java/org/now/sopt/sopt_kakaopay/service/ApiService.kt
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/service/ApiService.kt
@@ -1,12 +1,16 @@
 package org.now.sopt.sopt_kakaopay.service
 
+import org.now.sopt.sopt_kakaopay.MemberIdInterceptor
 import org.now.sopt.sopt_kakaopay.model.BookMarkRequestDto
 import org.now.sopt.sopt_kakaopay.model.BookMarkResponseDto
 import org.now.sopt.sopt_kakaopay.model.TransactionHistoryResponseDto
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("api/v1/history")
@@ -16,4 +20,11 @@ interface ApiService {
     suspend fun addBookmark(
         @Body bookmarkRequest: BookMarkRequestDto
     ): BookMarkResponseDto
+
+    @DELETE("api/v1/bookmark")
+    suspend fun deleteBookmark(
+        @Query("bank") bank: String,
+        @Query("bankAccount") bankAccount: String
+    ): BookMarkResponseDto
+
 }

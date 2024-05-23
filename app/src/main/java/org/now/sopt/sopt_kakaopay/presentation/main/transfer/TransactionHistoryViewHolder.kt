3df601283518ed127f1d29a8cd1fdd44f4c4ef68
yeonjeen <jeendev2019@gmail.com>
2024-05-23 11:20:43 +0900

    feat: 북마크 클릭리스너 이벤트 구현

--- app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/transfer/TransactionHistoryViewHolder.kt
+++ app/src/main/java/org/now/sopt/sopt_kakaopay/presentation/main/transfer/TransactionHistoryViewHolder.kt
@@ -1,16 +1,41 @@
 package org.now.sopt.sopt_kakaopay.presentation.main.transfer
 
 import androidx.recyclerview.widget.RecyclerView
+import org.now.sopt.sopt_kakaopay.R
 import org.now.sopt.sopt_kakaopay.databinding.ItemRecentAccountBinding
+import org.now.sopt.sopt_kakaopay.model.BookMarkRequestDto
 import org.now.sopt.sopt_kakaopay.model.TransactionHistoryDto
 
-class TransactionHistoryViewHolder(private val binding: ItemRecentAccountBinding): RecyclerView.ViewHolder(binding.root) {
+class TransactionHistoryViewHolder(
+    private val binding: ItemRecentAccountBinding,
+    private val viewModel: TransferViewModel
+) :
+    RecyclerView.ViewHolder(binding.root) {
     fun onBind(transactionHistory: TransactionHistoryDto) {
         with(binding) {
             tvRecentAccountUserName.text = transactionHistory.name
             tvRecentAccountUserNumber.text = transactionHistory.bankAccount
             tvRecentAccountUserBank.text = transactionHistory.bank
             ImageUtil.setBankImage(itemView.context, ivRecentUserBankLogo, transactionHistory.bank)
+
+            if (transactionHistory.bookmark) {
+                ivRecentUserFavorite.setImageResource(R.drawable.ic_star_full)
+            } else {
+                ivRecentUserFavorite.setImageResource(R.drawable.ic_star_empty)
+            }
+
+            ivRecentUserFavorite.setOnClickListener {
+                val bookmarkRequest =
+                    BookMarkRequestDto(transactionHistory.bank, transactionHistory.bankAccount)
+                if (transactionHistory.bookmark) {
+                    viewModel.deleteBookmark(bookmarkRequest)
+                } else {
+                    viewModel.addBookmark(bookmarkRequest)
+                }
+            }
+
         }
     }
+
 }
+
